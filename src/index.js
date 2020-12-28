fc51c0bb424ae782b3c222c7ab6406492aa11eb8
Jorge Sanchez <js7170206@gmail.com>
2020-12-27 19:33:59 -0600

    Install and configure nodemon to work with babel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@
 
 import express from "express"
 
+// Ahora utilizaremos el comando npx (que sirve para buscar unm comando en especifico en node_modules)
+// npx babel-node src/index.js
+// Pero para no escribir un comando tan largo creamos el siguiente script en el package.json
+// "start": "nodemon src/index.js --exec babel-node"
+
 const app = express();
 
 app.listen(3000);
