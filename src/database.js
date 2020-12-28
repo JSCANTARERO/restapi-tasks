09551b197c87da2385af174fb4e4c74cb9cd5b0b
Jorge Sanchez <js7170206@gmail.com>
2020-12-28 14:52:09 -0600

    Usage of dotenv for enviroment variables
    creation of config.js for manage configurations

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
-import mongoose, { connect } from 'mongoose'
+import mongoose, { connect } from 'mongoose';
+import config from './config'
 
 (async() => {
-    const db = await mongoose.connect('mongodb://localhost/tasksapi', {
+    const db = await mongoose.connect(config.mongodbURL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
